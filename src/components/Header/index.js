1573506c27e85677ce13f0015646e5baffae2c89
benyamin abedian <benyamin.abedian@gmail.com>
2020-08-18 11:15:44 +0200

    Modify the header to be consistent with the footer

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,10 +1,15 @@
 // @flow
 
 import React from 'react'
-import { Navbar, Nav } from 'react-bootstrap'
+import { Container, Navbar, Nav, Row } from 'react-bootstrap'
 import logo from '../../logo.png'
 
 const root = {
+    marginBottom: '24px',
+    borderBottom: 'solid 1px #ededed',
+}
+
+const navbar = {
     width: '100%',
     justifyContent: 'space-between',
     height: '80px',
@@ -21,23 +26,29 @@ const links = {
 export default function Header() {
     return (
         <header>
-            <Navbar collapseOnSelect style={root}>
-                <Navbar.Brand href="#home">
-                    <img src={logo} style={image} alt="logo" />
-                </Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <section style={links}>
-                    <Navbar.Collapse id="responsive-navbar-nav">
-                        <Nav>
-                            <Nav.Link href="#purchase">Purchase</Nav.Link>
-                            <Nav.Link href="#orders">My Orders</Nav.Link>
-                        </Nav>
-                        <Nav>
-                            <Nav.Link href="#deets">Sell</Nav.Link>
-                        </Nav>
-                    </Navbar.Collapse>
-                </section>
-            </Navbar>
+            <Row style={root}>
+                <Container>
+                    <Navbar collapseOnSelect style={navbar}>
+                        <Navbar.Brand href="#home">
+                            <img src={logo} style={image} alt="logo" />
+                        </Navbar.Brand>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        <section style={links}>
+                            <Navbar.Collapse id="responsive-navbar-nav">
+                                <Nav>
+                                    <Nav.Link href="#purchase">
+                                        Purchase
+                                    </Nav.Link>
+                                    <Nav.Link href="#orders">
+                                        My Orders
+                                    </Nav.Link>
+                                    <Nav.Link href="#deets">Sell</Nav.Link>
+                                </Nav>
+                            </Navbar.Collapse>
+                        </section>
+                    </Navbar>
+                </Container>
+            </Row>
         </header>
     )
 }
