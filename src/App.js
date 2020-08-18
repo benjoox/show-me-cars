a231eedf396d1ca021577f106c9529ef390a7560
benyamin abedian <benyamin.abedian@gmail.com>
2020-08-18 17:37:20 +0200

    stick footer to the bottom when page is loading

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@
 import React from 'react'
 import Header from './components/Header'
 import Footer from './components/Footer'
+import CollectionContainer from './components/CollectionContainer'
 
 function App() {
     return (
         <div>
-            <div style={{ height: '90vh' }}>
+            <div style={{ minHeight: 'calc(100vh - 80px)' }}>
                 <Header />
+                <CollectionContainer />
             </div>
             <Footer />
         </div>
